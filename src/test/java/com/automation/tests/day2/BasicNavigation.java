86fae15ab04110c761ae6827d7c08da6b240e5ee
garendell <g_arendell@yahoo.com>
2020-03-30 15:35:59 -0500

    created basic navigation class

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -20,6 +20,7 @@ public class BasicNavigation {
         // get is the first method, the key to open the door
         driver.get("http://google.com"); //to open website
         //now you can send this to github by clicking the green check mark at the top right.
+        //it didn't work - his shows a push button, mine doesn't.  try again.
 
     }
 }
