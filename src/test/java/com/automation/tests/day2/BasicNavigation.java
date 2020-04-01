625b33cdae894f52bd77a19ffe4c1afbdee76656
garendell <g_arendell@yahoo.com>
2020-03-31 20:33:35 -0500

    created basic navigation class

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -6,7 +6,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BasicNavigation {
     public static void main(String[] args) throws InterruptedException {
-        /* to start selenium script we need:
+
+        /* to start a selenium script we need to:
         set up webdriver (browser driver) and
         create webdriver object
          */
@@ -22,9 +23,20 @@ public class BasicNavigation {
         // get is the first method, the key to open the door
 
         driver.get("http://google.com"); //to open website
+        /*
+        there are other commands for back forward and refresh, and navigate to other site,
+        like this:
+        driver.navigate().to(url);  //for example, navigate to (amazon)
+        driver.navigate().back();    // for example, you could use this to go back to google after amz
+        driver.navigate().forward();   //the same as the arrows, back, forward, refresh...
+        driver.navigate().refresh();
+         */
+
+        //if you want to open in maximized or in full screen, you can do this:
+        driver.manage().window().maximize();
 
         //now you can send this to github by clicking the green check mark at the top right.
-        //it didn't work - his shows a push button, mine doesn't.  try again. had to use push command.
+        //it didn't work - his shows a push button, mine doesn't.  try again. **there's an option to commit AND PUSH,
 
         Thread.sleep(3000);
 
@@ -37,9 +49,25 @@ public class BasicNavigation {
         } else {
             System.out.println("Test Failed");
         }
+        //ok, let's try going back to google, then going to Amz, in same window:
+        driver.navigate().back();
+        driver.navigate().to("http://amazon.com");
+        if(driver.getTitle().toLowerCase().contains("Amazon")  ){
+            System.out.println("Test Passed");
+        }else {
+            System.out.println("Test Failed");
+        }
         driver.close(); //to close browser
 
      //troubleshoot:  if you have any issues with running this, first try updating Chrome and updating Intellij.
-//try to commit to github
+        //try to commit to github
     }
+    public static void verifyEquals(String arg1, String arg2){
+        if(  arg1.equals(arg2) ) {
+            System.out.println("Test Passed");
+        }else{
+            System.out.println("Test Failed");
+        }
+    }
+
 }
