b8961ee4d4c49f271181d5bd42a62e3e277f3468
garendell <g_arendell@yahoo.com>
2020-04-01 08:08:28 -0500

    created basic navigation class

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -26,7 +26,7 @@ public class BasicNavigation {
         /*
         there are other commands for back forward and refresh, and navigate to other site,
         like this:
-        driver.navigate().to(url);  //for example, navigate to (amazon)
+        driver.navigate().to(url);  //for example, navigate to (amazon)  **** this does the same thing as driver.get !!*****
         driver.navigate().back();    // for example, you could use this to go back to google after amz
         driver.navigate().forward();   //the same as the arrows, back, forward, refresh...
         driver.navigate().refresh();
@@ -42,32 +42,57 @@ public class BasicNavigation {
 
         String title = driver.getTitle();  //method that returns title of the page
         String expectedTitle = "Google";
-        System.out.println("title is...:" +title);
+        System.out.println("title is...:" + title);
 
-        if(expectedTitle.equals(title) ){
-            System.out.println("Test Passed");
+        if (expectedTitle.equals(title)) {
+            System.out.println("title contains google: Test Passed");
         } else {
-            System.out.println("Test Failed");
+            System.out.println("title contains google: Test Failed");
         }
         //ok, let's try going back to google, then going to Amz, in same window:
         driver.navigate().back();
+        Thread.sleep(3000);
+
         driver.navigate().to("http://amazon.com");
-        if(driver.getTitle().toLowerCase().contains("Amazon")  ){
-            System.out.println("Test Passed");
-        }else {
-            System.out.println("Test Failed");
+        Thread.sleep(5000);
+        String title2 = driver.getTitle();
+        System.out.println("title is...:" + title2);
+        if (driver.getTitle().contains("Amazon")) {
+            System.out.println("title contains Amz: Test Passed");
+        } else {
+            System.out.println("title contains Amz: Test Failed");
         }
+        //go back to google
+        driver.navigate().back();
+        Thread.sleep(3000);
+        verifyEquals(driver.getTitle(), "Google");
+        System.out.println("Yes, it went back to Google - idk why this prints out " +
+                "AFTER the args equal test???");
+        //if you want to move forward in the browser history (return to Amz):
+        driver.navigate().forward();
+        Thread.sleep(3000);
+        System.out.println("Now it moved forward to (Title:) "+ driver.getTitle() );
+
+        driver.navigate().refresh(); //just to demo all the navigate options, let's do refresh too.
+
+        driver.getCurrentUrl();
+        Thread.sleep(3000);
+        System.out.println("current url:" +driver.getCurrentUrl()  );
+
+
         driver.close(); //to close browser
 
-     //troubleshoot:  if you have any issues with running this, first try updating Chrome and updating Intellij.
-        //try to commit to github
     }
     public static void verifyEquals(String arg1, String arg2){
         if(  arg1.equals(arg2) ) {
-            System.out.println("Test Passed");
+            System.out.println("args equal: Test Passed");
         }else{
-            System.out.println("Test Failed");
+            System.out.println("args equal: Test Failed");
         }
-    }
 
+    }
 }
+
+//troubleshoot:  if you have any issues with running this, first try updating Chrome and updating Intellij.
+//try to commit to github
+
