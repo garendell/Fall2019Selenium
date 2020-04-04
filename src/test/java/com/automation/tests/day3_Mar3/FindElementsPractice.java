05e71b1fe9e344b37477d4eb75e917a8ff1f7c08
garendell <g_arendell@yahoo.com>
2020-04-03 21:03:07 -0500

    created basic navigation class

---
+++ src/test/java/com/automation/tests/day3_Mar3/FindElementsPractice.java
@@ -0,0 +1,20 @@
+package com.automation.tests.day3_Mar3;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FindElementsPractice {
+    public static void main(String[] args) {
+
+        /*create a method instead of webdrivermanager.chromedriver setup...
+       WebElement driver = (WebElement) new ChromeDriver();
+       // open a utilites package under automation
+       // tests package - is where we will store all the tests
+       // utilities package - is where we will store all the reusable methods
+        that will support our tests.  For Example, we can create a class
+        with a method that will give us a Webdriver whenever we need it.
+         //Then create a class under utilities called DriverFactory
+         */
+
+    }
+}
---
+++ src/test/java/com/automation/utilities/DriverFactory.java
@@ -0,0 +1,24 @@
+package com.automation.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DriverFactory {
+
+    /*This method returns webdriver object based on browser type -
+    if you want to use chrome browser, provide chrome as parameter.
+    and RETURN webdriver object
+     */
+    public static WebDriver createADriver(String browserName) {
+        if (browserName.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            return new ChromeDriver();
+        } else {
+            WebDriverManager.firefoxdriver().setup();
+            return new FirefoxDriver();
+
+        }
+    }
+}
