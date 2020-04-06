d7fe8ff619bd44892a1d035c013898b720cd71ef
garendell <g_arendell@yahoo.com>
2020-04-05 21:10:25 -0500

    created basic navigation class

---
+++ src/test/java/com/automation/tests/day3_Mar3/FindElementById.java
@@ -0,0 +1,79 @@
+package com.automation.tests.day3_Mar3;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FindElementById {
+    public static void main(String[] args) throws Exception {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://practice.cybertekschool.com/login");
+
+        // WebElement username =  // this part is not necessary because???
+        Thread.sleep(3000);
+        driver.findElement(By.name("username")).sendKeys("tomsmith");
+        Thread.sleep(3000);
+        driver.findElement(By.name("password")).sendKeys("SuperSecretPassword");
+        Thread.sleep(3000);
+        driver.findElement(By.id("wooden_spoon")).click();
+        Thread.sleep(3000);
+
+        String expected = "Welcome to the Secure Area. When you are done click logout below.";
+        String actual = driver.findElement(By.tagName("h4")).getText();
+        if (expected.equals(actual)) {
+            System.out.println("Login test Passed");
+        } else {
+            System.out.println("Login test Failed");
+        }
+
+        //now let's logout.  it's difficult to figure out how to find the logout button
+        //because it is tagged as <a   which means a link.
+        // it looks like a button, but it's a link.
+        //tips about using the cntrl-F search bar:
+        // to find attribute like name, id, class, just pub the
+        //attri3bute=value pair in square brackets[].  [name="password]
+        // a button image on the page may be a <button> or it may be a link <a.
+        //we can find the linked text like this (using the visible text):
+
+        WebElement logout = driver.findElement(By.linkText("Logout"));
+        //In this test, there was a question about a space in the code.
+        // If there is any question about the exact wording or spacing of a linktext,
+        // you can use partialLinkText --
+        // linkText is like .equals() ; partialLinkText is like .contains() ;
+        Thread.sleep(3000);
+        logout.click();
+        Thread.sleep(3000);
+        // getAttribute - returns attribute value
+        // <a class = "button secondary radius" href= "/logout>
+        // <i class = "icon-2x icon-signout"> Logout</i> </a>
+        // a - is a tag name; class is an attribute; href is an attribute;
+        // logout is visible text.
+        //  if you say getAttribute("class:) it will return "button secondary radius"
+        //  "make sure this attribute is present in the element" ... ????
+
+        // let's do a negative test (invalid credentials)
+        // set up the exact same code as before, but put in an invalid word in sendKeys:
+        driver.findElement(By.name("username")).sendKeys("invalid username");
+        Thread.sleep(3000);
+        driver.findElement(By.name("password")).sendKeys("invalid password");
+        Thread.sleep(3000);
+        driver.findElement(By.id("wooden_spoon")).click();
+        Thread.sleep(3000);
+
+//   idk why these two lines are here - i think i missed the last minutes:
+//   String expected = "Welcome to the Secure Area. When you are done click logout below.";
+//   String actual = driver.findElement(By.tagName("h4")).getText();
+
+        if (expected.equals(actual)) {
+            System.out.println("Login test Passed");
+        } else {
+            System.out.println("Login test Failed");
+
+            driver.quit();
+
+        }
+    }
+}
--- src/test/java/com/automation/tests/day3_Mar3/FindElementsPractice.java
+++ src/test/java/com/automation/tests/day3_Mar3/FindElementsPractice.java
@@ -10,11 +10,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class FindElementsPractice  {
     public static void main(String[] args) throws Exception{
 
-        //create a method instead of webdrivermanager.chromedriver setup... do this:
+        //create a method to use instead of always repeating webdrivermanager.chromedriver setup...
       // WebElement driver = (WebElement) new ChromeDriver();
+        //and put it in a "utilities" package under "tests"
 
         /*
-        // open a utilites package under automation
+        // open a utilities package under automation
        // tests package - is where we will store all the tests
        // utilities package - is where we will store all the reusable methods
         that will support our tests.  For Example, we can create a class
@@ -24,37 +25,58 @@ public class FindElementsPractice  {
 
         WebDriver driver = DriverFactory.createADriver("chrome");
 
-             //this is only one line of code instead of two, because we set up this
-                //method in the DriverFactory utility package.  We only have to change
+             //this results in only one line of code instead of two, and it's reusable
+            //we now have this method in the DriverFactory utility package.  We only have to change
                 //the BROWSER NAME in the parameters to create the driver we need.
 
         driver.get("http://practice.cybertekschool.com/sign_up");
-                            //our code goes between get and quit... obviously.
+                            // after creating the driver, tell the driver to get the URL you want
+                            // our code will be written between get and quit... obviously.
                             // all Vasyl's codes are available on the github
 
-            //now we want to automate the sign up process.
-            // we need to tell the driver to
-            // find the webelement for the signup box, enter name,
-            // then find the email box, enter email,
-            //then click the sign up button.
+            // we are using the sign up page of the cybertek practice site
+            // we want to automate the signup process??  or test that it is working right? not sure yet
+            // we need to tell the driver to:
+                // find the webelement for the signup box,
+                // enter the person's name,
+                // then find the email box, enter email,
+                //then click the sign up button.
 
-        WebElement fullName = driver.findElement(By.name("full_name")  );
+
       /*  This is my guess about what we will do:
-        driver.enterText("name"); wrong - the command is sendKeys
-        driver.findElement(By.name("email")  ); ok, but not just driver, use WebElement
-        driver.enterText ("email@email.com"); - wrong - use sendKeys
-        driver.findElement(By.name ("sign up button") );
+        driver.enterText("name");       wrong - the command is sendKeys
+        driver.findElement(By.name("email")  );     ok, but not just driver, use WebElement command first, then tell the driver what to find
+        driver.enterText ("email@email.com");   - wrong - not enterText,  use sendKeys
+        driver.findElement(By.name ("sign up button") );  yes, but signup button is not available by name, we have to use
         driver.click("sign up button");
       This is what we actually do:   */
-      fullName.sendKeys("Mister Twister");
+        WebElement fullName = driver.findElement(By.name("full_name")  );
+        fullName.sendKeys("Mister Twister");
         WebElement email = driver.findElement(By.name("email")  );
         email.sendKeys(" sdet@cybertek.com");
         WebElement signup = driver.findElement(By.name("wooden_spoon")  );
+                                                        //use control F to check for uniqueness of locator
+                                                        // we discover that there are 3 unique ways to find this element
+                                                        //it can be located using  tag name (h3), name, or class name
         Thread.sleep(5000);
-        signup.click();  //or you could use "signup.submit" - more common to use click,
-                            //but sometimes click doesn't work, so then you can use submit.
-       driver.quit(); //closes everything
 
+        signup.click();  //or you could use "signup.submit" - more common to use click,
+        //but sometimes click doesn't work, so then you can use submit.
+        Thread.sleep(3000);
+                              //now we need to verify that it goes to the final message screen after submission.
+                              // we say what message is expected, by entering exact text as expected WebElement
+                             // then we set up a WebElement search for that message, using either tag name, name, or class name
+        String expected = "Thank you for signing up. Click the button below to return to the home page.";
+        WebElement message = driver.findElement(By.className("subheader") );
+        String actual = message.getText();
+        if(expected.equals(actual) ){
+            System.out.println("Signup test Passed");
+        }else{
+            System.out.println("Signup test Failed");
+        }
+        Thread.sleep(3000);
+
+        driver.quit(); //closes everything
 
     }
 }
